49bf5664e8b9ede832bb8d668f036b687bafbe7c
Flavia Ferreira <flavia.fau08@gmail.com>
2019-09-27 12:56:30 -0300

    adicionando sobreposicao

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -3,22 +3,29 @@ package entities;
 public class SavingsAccount extends Account {
 
 	private Double interestRate;
+
 	public SavingsAccount() {
-	super();
-}
+		super();
+	}
+
 	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
 		super(number, holder, balance);
 		this.interestRate = interestRate;
 	}
+
 	public Double getInterestRate() {
 		return interestRate;
 	}
+
 	public void setInterestRate(Double interestRate) {
 		this.interestRate = interestRate;
 	}
-public void updateBalance() {
-	balance += balance * interestRate;
-}
-	}
-	
 
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+}
